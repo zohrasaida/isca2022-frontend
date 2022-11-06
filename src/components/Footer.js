56a4f2ac250918899bd19836573239bd4e8d35c9
zohrasaida <saidazohra98@gmail.com>
2022-11-06 19:00:17 +0100

    frontEnd architecture'

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './footer.css';
+
+const Footer = () => {
+  
+    return (
+      <footer className="footer">
+        <div className="d-sm-flex justify-content-center justify-content-sm-between" > 
+          <span className="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © Ensit Junior Enterprise</span>
+        </div>
+      </footer>
+    );
+  
+}
+
+export default Footer;
